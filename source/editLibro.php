6777a13f948ec02a0c0ae53565c9b8c574f8bdc7
Magin Luna <magin1104@gmail.com>
2021-11-29 16:01:48 -0400

    TO UPPER

--- source/editLibro.php
+++ source/editLibro.php
@@ -5,9 +5,9 @@
   }else{
 
     $sqllibro= "UPDATE libro SET categoria='".$_POST['cat'].
-               "',titulo='".$_POST['tit'].
-               "',autor='".$_POST['aut'].
-               "',editorial='".$_POST['edi'].
+               "',titulo='". strtoupper($_POST['tit']).
+               "',autor='".strtoupper($_POST['aut']).
+               "',editorial='".strtoupper($_POST['edi']).
                "',anio_publicacion=".$_POST['ani'].
                " WHERE cod_libro=".$_POST['cod']; 
     $reslibro=$conexion->query($sqllibro);
